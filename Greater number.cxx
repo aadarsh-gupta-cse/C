bfc750105f2e3580d10928456d2637e1e10d717d
aadarsh-gupta-cse <guptaaadarsh195@gmail.com>
2026-09-07 17:10:57 +0530

    Add files via upload

---
+++ Greater number.cxx
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main()
+{
+	int x,y;
+	printf("Enter first number:");
+	scanf("%d",&x);
+	printf("Enter second number:");
+	scanf("%d",&y);
+	if(x>y)
+	{
+		printf("x is greater than y");
+	}
+	else if("y>x")
+	{
+		printf("y is greater than x");
+	}
+	return 0;
+}
